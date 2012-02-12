076283aa0239a38c70caad0ba44488ec40776725
Adrian Smith <smith.adriane@gmail.com>
2012-02-11 16:17:33 -0800

    added xcode proj.

---
+++ gimpleTests/gimpleTests.h
@@ -0,0 +1,13 @@
+//
+//  gimpleTests.h
+//  gimpleTests
+//
+//  Created by Adrian Smith on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface gimpleTests : SenTestCase
+
+@end
---
+++ gimpleTests/gimpleTests.m
@@ -0,0 +1,32 @@
+//
+//  gimpleTests.m
+//  gimpleTests
+//
+//  Created by Adrian Smith on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "gimpleTests.h"
+
+@implementation gimpleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in gimpleTests");
+}
+
+@end
